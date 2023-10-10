82fde1937939e96da8857d7312585be4afa486fc
Snehal <snehal.nikam@fiserv.com>
2023-10-10 12:48:03 +0530

    first commit

---
+++ MyFisrtNetCoreWebApp/Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFisrtNetCoreWebApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFisrtNetCoreWebApp.Controllers
+{
+    public class LocationController : Controller
+    {
+        ILog _log;
+        public LocationController(ILog log)
+        {
+            _log = log;
+        }
+        public IActionResult Index()
+        {
+            _log.Info("Executing /Location");
+            return View();
+        }
+    }
+}
---
+++ MyFisrtNetCoreWebApp/Models/LocationDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFisrtNetCoreWebApp.Models
+{
+    public class LocationDetails
+    {
+        public string  Company{ get; set; }
+
+        public string Area{ get; set; }
+
+        public string City{ get; set; }
+    }
+}
